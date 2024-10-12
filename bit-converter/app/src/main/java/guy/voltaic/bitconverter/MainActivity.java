54cb96c389df89a7dd688684734bfa7b49565366
robisonsantana <robsonsantana9107@gmail.com>
2024-10-12 20:15:06 -0300

    Exercícios concluídos

--- bit-converter/app/src/main/java/guy/voltaic/bitconverter/MainActivity.java
+++ bit-converter/app/src/main/java/guy/voltaic/bitconverter/MainActivity.java
@@ -12,6 +12,10 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+/*
+ *@author:<Robison>
+ *@ra:<1110482313007>
+ */
 public class MainActivity extends AppCompatActivity {
 
     private EditText etBit;
@@ -41,8 +45,31 @@ public class MainActivity extends AppCompatActivity {
         bntCalc.setOnClickListener(op -> calc());
     }
 
+    /*
+     *@author:<Robison>
+     *@ra:<1110482313007>
+     */
     private void calc() {
-        int num = Integer.parseInt(etBit.getText().toString());
+        int bits = Integer.parseInt(etBit.getText().toString());
+        double result = 0;
         
+        if(rbBytes.isChecked()) {
+            result = bits / 8.0;
+            tvRes.setText(String.valueOf(result) + "Bytes");
+        } else if(rbKb.isChecked()) {
+            result = bits / (8.0 * 1024);
+            tvRes.setText(String.valueOf(result) + "KB");
+        } else if(rbMb.isChecked()) {
+            result = bits / (8.0 * 1024 * 1024);
+            tvRes.setText(String.valueOf(result) + "MB");
+        } else if(rbGb.isChecked()) {
+            result = bits / (8.0 * 1024 * 1024 * 1024);
+            tvRes.setText(String.valueOf(result) + "GB");
+        } else if(rbTb.isChecked()) {
+            result = bits / (8.0 * 1024 * 1024 * 1024 * 1024);
+            tvRes.setText(String.valueOf(result) + "TB");
+        }
+
+        etBit.setText("");
     }
 }
